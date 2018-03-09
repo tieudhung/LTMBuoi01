7e03f7be40d04113e898ac4b4bcee3b03c431d96
hungtieu <tieud.hung@gmail.com>
2018-03-09 15:32:29 +0700

    bai1 + bai 2

---
+++ Bai1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string hostName = Dns.GetHostName();
+            IPHostEntry ipHostEntry = Dns.Resolve(hostName);
+            Console.WriteLine(ipHostEntry.HostName);
+            foreach ( IPAddress s in ipHostEntry.AddressList)
+            {
+                Console.WriteLine(s);
+            }
+
+        }
+    }
+}
---
+++ Bai2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+
+
+namespace Bai2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Nhap ten mien: ");
+            string hostName = Console.ReadLine();
+            IPHostEntry ipHostEntry= Dns.Resolve(hostName);
+            Console.WriteLine(ipHostEntry.HostName);
+            foreach (IPAddress s in ipHostEntry.AddressList)
+            {
+                Console.WriteLine(s);
+            }
+        }
+    }
+}
